17e9384b21b83f7423fab40051828d250c1d1e27
JuanAntonioPanchon <martin.panchon.juan.antonio@iescamas.es>
2024-10-04 10:03:04 +0200

    borrado el dolar y corregido el error para que se vea el indice (faltaba una s en la variable que guarda los libros)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include 'config.php';
 
 $stmt = $pdo->query('SELECT * FROM libros');
-$libro = $stmt->fetchAll();
+$libros = $stmt->fetchAll();
 ?>
 
 <h2>Listado de Libros</h2>
@@ -14,7 +14,7 @@ $libro = $stmt->fetchAll();
 <ul>
 <?php foreach ($libros as $libro): ?>
     <li>
-        <?php echo $libro['nombre']; ?> - $<?php echo $libro['precio'];?> - $<?php echo $libro['autor']; ?>
+        <?php echo $libro['nombre']; ?> - <?php echo $libro['precio']; ?> - <?php echo $libro['autor'] ?>
         <a href="edit.php?id=<?php echo $libro['id']; ?>">Editar</a>
         <a href="delete.php?id=<?php echo $libro['id']; ?>">Eliminar</a>
     </li>
